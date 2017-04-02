53a123528e2dbf39ac2bce6384ecaa972193bf2e
Tomdozz <tom_1995@msn.com>
2017-04-02 22:05:51 +0200

    #9 particle engine
    More work with particle engine

--- JustForFun/Particles/Particle.cs
+++ JustForFun/Particles/Particle.cs
@@ -11,8 +11,13 @@ namespace JustForFun.Particles
     class Particle
     {
         public Texture2D m_Texture;
+        public Color m_Color;
+
         public Vector2 m_Position;
+        public Vector2 m_Scale = Vector2.One;
 
-
+        public float m_Orientation;
+        public float m_Duration;
+        public float m_PercentLife = 1f;
     }
 }
--- JustForFun/Particles/ParticleManager.cs
+++ JustForFun/Particles/ParticleManager.cs
@@ -8,5 +8,49 @@ namespace JustForFun.Particles
 {
     class ParticleManager
     {
+        /// <summary>
+        /// Fattar inte läs sen
+        /// </summary>
+        private class RotatetParticleArray
+        {
+            private int m_Start;
+
+            public int Start
+            {
+                get { return m_Start; }
+                set { m_Start = value % list.Length; }
+            }
+
+            //Active particles
+            public int Count { get; set; }
+            public int Capacity { get { return list.Length; } }
+            private Particle[] list;
+
+            public RotatetParticleArray(int capacity)
+            {
+                list = new Particle[capacity];
+            }
+
+            public Particle this[int i]
+            {
+                get { return list[(m_Start + i) % list.Length]; }
+                set { list[(m_Start + i) % list.Length] = value; }
+            }
+        }
+
+        private Particle[] m_ParticleList;
+
+        public ParticleManager(int capacity)
+        {
+            for (int i = 0; i < capacity; i++)
+            {
+                m_ParticleList[i] = new Particle();
+            }
+        }
+
+        public void CreateParticle()
+        {
+
+        }
     }
 }
